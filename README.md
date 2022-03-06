726803665797dedb806c6ca3a8502f0261e5beac
Javier Luraschi <jluraschi@gmail.com>
2022-03-06 02:06:42 -0800

    initial commit

---
+++ README.md
@@ -0,0 +1,9 @@
+# Love not War
+
+<b>lovenotwar.js</b> is a library that stands for peace in the world, love not war.
+
+```js
+<script src="https://cdn.jsdelivr.net/npm/lovenotwar@latest/dist/lovenotwar.min.js"></script>
+```
+
+Contributions are very welcomed!
--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,20 @@
+const path = require('path');
+const UglifyJsPlugin = require('uglifyjs-webpack-plugin');
+
 module.exports = {
-  "output": {
-    "library": "notcool",
-    "filename": "notcool.js"
+  entry: {
+    'lovenotwar': './src/index.js',
+    'lovenotwar.min': './src/index.js',
   },
+  devtool: "source-map",
+  output: {
+    filename: '[name].js',
+    path: path.resolve(__dirname, 'dist'),
+  },
+  optimization: {
+    minimize: false,
+    minimizer: [new UglifyJsPlugin({
+      include: /\.min\.js$/
+    })]
+  }
 }
